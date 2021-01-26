a2db395438bf7174ca5a74cd0453ecdc5475f702
kryptn <kryptn@gmail.com>
2021-01-26 00:05:52 -0700

    ...

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # infra
+
+install [taskfile](https://taskfile.dev/#/installation)
+
+```
+task cluster:create
+
+
+```
--- do-cluster/cluster.tf
+++ do-cluster/cluster.tf
@@ -16,19 +16,19 @@ resource "digitalocean_kubernetes_cluster" "arq" {
 
 }
 
-resource "digitalocean_record" "www" {
-    domain = "arq.dev"
-    type = "A"
-    name = "www"
-    value = "134.209.143.84"
-}
+# resource "digitalocean_record" "www" {
+#     domain = "arq.dev"
+#     type = "A"
+#     name = "www"
+#     value = "134.209.143.84"
+# }
 
-resource "digitalocean_record" "self" {
-    domain = "arq.dev"
-    type = "A"
-    name = "@"
-    value = "134.209.143.84"
-}
+# resource "digitalocean_record" "self" {
+#     domain = "arq.dev"
+#     type = "A"
+#     name = "@"
+#     value = "134.209.143.84"
+# }
 
 output "kubeconfig" {
   value = digitalocean_kubernetes_cluster.arq.kube_config.0.raw_config
