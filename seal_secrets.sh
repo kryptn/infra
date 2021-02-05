80c9fbfeacfd66b42c316e464776d2998ab03274
kryptn <kryptn@gmail.com>
2021-02-05 00:13:42 -0700

    add websub

--- seal_secrets.sh
+++ seal_secrets.sh
@@ -11,3 +11,7 @@ kubectl -n cert-manager create secret generic cert-manager-digitalocean-token \
 kubectl -n external-dns create secret generic external-dns-digitalocean-token \
   --dry-run=client --from-env-file=secrets/do_token.env -o json > secrets/do_token-edns.env.json \
   | kubeseal --controller-namespace sealed-secrets > apps/external-dns/sealed-do-token.json
+
+kubectl -n websub-listener create secret generic websub-listener-config-secret \
+  --dry-run=client --from-file=config.toml=secrets/websub_listener_config.toml -o json > secrets/websub_listener_config.toml.json \
+  | kubeseal --controller-namespace sealed-secrets > apps/websub-listener/sealed-websub-config.json
