689413bba3c346a35f9cbc48f749623cfbb76bf6
kryptn <kryptn@gmail.com>
2021-08-06 14:53:10 -0600

    added/rolled secrets

--- seal_secrets.sh
+++ seal_secrets.sh
@@ -4,6 +4,10 @@ kubectl -n argocd create secret generic argo-cd-github-token \
   --dry-run=client --from-env-file=secrets/github_token.env -o json > secrets/github_token.env.json \
   | kubeseal --controller-namespace sealed-secrets > apps/argo-cd/sealed-gh-token.json
 
+kubectl -n argocd create secret generic argo-cd-github-odic-token \
+  --dry-run=client --from-env-file=secrets/github_odic_client_secret.env -o json > secrets/github_odic_client_secret.env.json \
+  | kubeseal --controller-namespace sealed-secrets > apps/argo-cd/sealed-gh-odic-secret.json
+
 kubectl -n cert-manager create secret generic cert-manager-digitalocean-token \
   --dry-run=client --from-env-file=secrets/do_token.env -o json > secrets/do_token-cm.env.json \
   | kubeseal --controller-namespace sealed-secrets > apps/cert-manager/sealed-do-token.json
