9c10db3b485c933c45978eae9c7ed950595ae5b8
kryptn <kryptn@gmail.com>
2021-08-29 16:59:54 -0600

    automate deploy

--- cloud/cluster.tf
+++ cloud/cluster.tf
@@ -2,13 +2,15 @@ resource "digitalocean_kubernetes_cluster" "arq" {
   name   = var.cluster_name
   region = var.do_region
   # Grab the latest version slug from `doctl kubernetes options versions`
-  version = "1.19.3-do.3"
+  version = "1.19.12-do.0"
   # tags    = ["staging"]
 
+  # auto_upgrade  = true
+  surge_upgrade = true
   node_pool {
     name       = "worker-pool"
     size       = "s-2vcpu-2gb"
-    node_count = 3
+    node_count = 4
   }
 
 }
